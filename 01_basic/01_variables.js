e9ab4d7cdd6d190ac2a6f9843d82cac2dc810a84
MD SMMI ANSARI <163724152+Md-Smmi-Ansari@users.noreply.github.com>
2024-03-31 06:13:52 +0000

    container configure two

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 122553
+let accountEmail = "smmi@google.com"
+var accountPassword = "12345"
+accountCity = "jaiput"
+
+//accountId = 2 //not allwed 
+
+accountEmail = "sm@sm.com"
+accountPassword = "21212121"
+accountCity = "Bengalur"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
